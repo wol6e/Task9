960d8703fe0253b9e110848328c5a015701385c7
Liudmila <yaveryvz@mail.ru>
2023-07-16 11:23:58 +0300

    add numbers

---
+++ number64/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте число N. Напишите программу, которая выведёт все натуральные чётные
+// числа от N до 1
+
+int number = Prompt("Введите число: ");
+Console.WriteLine($"{Range(number)}");
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+string Range(int n)
+{
+    if (n % 2 == 0)       
+    {
+       if( n == 2)
+          {
+           return "2";
+          }
+       return n + ", " + Range(n-2); 
+    }
+    else 
+    {
+        if (n == 2)
+           {
+            return "2";
+           }
+        return (n - 1) + ", " + Range(n-2);
+    }
+}
+
+//Почему программа не работает с нечётными числами?
---
+++ number66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов от M до N
+// M = 4, N = 8 -> 30
+
+int n = Prompt("Введите большее число: ");
+int m = Prompt("Введите меньшее число: ");
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+Console.Write(SumMN(m-1, n));
